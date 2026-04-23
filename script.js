4de64c6c1192cb592acb1a3f236ef96062414877
antonilukjan2010-hub <antonilukjan2010@gmail.com>
2026-04-23 21:42:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -47,8 +47,77 @@ const wynikzysku = document.querySelector(".wynikzysku")
 const obliczzysk = document.querySelector(".obliczzysk")
 const wynik = (przychód - podatek -  koszt)
 wynikzysku.textContent = "Zysk " + wynik
+
+
+
 })
-localStorage.setItem("user", 40)
-console.log(localStorage.getItem("user")) 
+let sales = JSON.parse(localStorage.getItem("sales")) || []
+obliczzysk.addEventListener("click", function(){
+    const przychód = parseInt(document.querySelector(".przychód").value)
+const koszt = parseInt(document.querySelector(".koszt").value)
+const podatek = 0.12 * przychód
+const wynikzysku = document.querySelector(".wynikzysku")
+const obliczzysk = document.querySelector(".obliczzysk")
+const wynik = (przychód - podatek -  koszt)
+const container2 = document.querySelector(".container2")
+wynikzysku.textContent = "Zysk " + wynik
+       sales.push(wynik)
+       localStorage.setItem("sales", JSON.stringify(sales))
+       const suma = sales.reduce((acc, curr) => acc + curr, 0 )
+       console.log(suma)
+       container2.textContent = suma.toFixed(2) + " PLN"
+})
+const container2 = document.querySelector(".container2")
+const cofnij = document.querySelector(".cofnij")
+cofnij.addEventListener("click", function(){
+    sales.pop()
+    localStorage.setItem("sales", JSON.stringify(sales))
+    const suma = sales.reduce((acc, curr) => acc + curr, 0 )
+    container2.textContent = suma.toFixed(1) + " PLN"
+    function animacja1() {
+        container2.classList.toggle("animacja1")
+    }
+
+})
+
+const style = document.querySelector(".style")
+
+function zmiana() {
+    style.classList.toggle("zmiana")
+}
+style.addEventListener("click", zmiana)
+
+function ankieta() {
+    const values = [
+        document.querySelector("#input1").value,
+        document.querySelector("#input2").value,
+        document.querySelector("#input3").value,
+        document.querySelector("#input4").value,
+        document.querySelector("#input5").value,
+        document.querySelector("#input6").value,
+        document.querySelector("#input7").value
+    ];
+
+    const bars = document.querySelectorAll(".bar");
+
+    values.forEach((val, i) => {
+        const height = Number(val) * 5 || 0;
+        bars[i].style.height = height + "px";
+    });
+}
 
 
+
+function rysuj(){
+    const values = [
+        document.querySelector("#val1"),
+        document.querySelector("#val2"),
+        document.querySelector("#val3"),
+        document.querySelector("#val4")
+    ]
+
+    values.forEach((val, i) => {
+        const height = val * 2;
+        document.querySelector(".bar" + (i + 1)).style.height = height + "px"
+    }
+)}
